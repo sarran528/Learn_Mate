cdf5acd7d3298f3d53075d3e0b0281246f862c72
sarranm528 <sarranm528@gmail.com>
2025-08-27 09:21:16 +0000

    vite fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
